b57f6758d9af864b690eb9103227d28d0270e987
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-07-04 16:07:21 -0700

    Unique Paths II

---
+++ Unique Paths II/Method.cpp
@@ -0,0 +1,42 @@
+//Follow up for "Unique Paths":
+//
+//Now consider if some obstacles are added to the grids. How many unique paths would there be?
+//
+//An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+//
+//For example,
+//There is one obstacle in the middle of a 3x3 grid as illustrated below.
+//
+//[
+//  [0,0,0],
+//  [0,1,0],
+//  [0,0,0]
+//]
+//The total number of unique paths is 2.
+//
+//Note: m and n will be at most 100.
+
+
+//和I思路几乎一样,之要看到obstacle 填0就好了，否则还是用一样的递推公式。
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        obstacleGrid[0][0] =  obstacleGrid[0][0]? 0:1;
+        for(int i = 1; i < obstacleGrid[0].size(); ++i) {
+            obstacleGrid[0][i] = obstacleGrid[0][i] ? 0: obstacleGrid[0][i-1];
+        }
+        
+        for(int i = 1; i < obstacleGrid.size(); ++i) {
+            obstacleGrid[i][0] = obstacleGrid[i][0] ? 0: obstacleGrid[i-1][0];
+        }
+        
+        for(int i = 1; i < obstacleGrid.size(); ++i) {
+            for(int j = 1; j < obstacleGrid[0].size(); ++j) {
+                obstacleGrid[i][j] = obstacleGrid[i][j] ? 0: obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+            }
+        }
+        return obstacleGrid.back().back();
+    }
+};
