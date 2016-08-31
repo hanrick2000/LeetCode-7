c04b2b9948a67bd8b9160f503c6901104278f90a
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-08-30 22:32:20 -0700

    Find Minimum in Rotated Sorted Array

---
+++ Find Minimum in Rotated Sorted Array/Method.cpp
@@ -0,0 +1,39 @@
+//Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+//
+//(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+//
+//Find the minimum element.
+//
+//You may assume no duplicate exists in the array.
+
+//二分法，找到根据断点左还是在右进行递归，不过还得注意可能没有转过，就是第一个值。一定要注意递归出口，否则会死循环。
+
+class Solution {
+public:
+    
+    int findMinHelper(vector<int>& nums, int start, int end) {
+        
+        if(start+1==end) return nums[start];
+        
+        if(start + 2 == end) return min(nums[start],nums[start+1]);
+        
+        int mid = (start + end)/2;
+        
+        if(nums[start] > nums[mid]&&nums[mid]<nums[end-1]) {
+            return findMinHelper(nums,start,mid+1);
+        }
+        
+        if(nums[start]<nums[mid]&&nums[mid]>nums[end-1]) {
+            return findMinHelper(nums, mid, end);
+        }
+        
+        return nums[start];
+        
+    }
+
+    int findMin(vector<int>& nums) {
+        
+        return findMinHelper(nums, 0, nums.size());
+
+    }
+};
