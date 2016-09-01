a466f5bb724dd8a3b418e17f52856cb1bb13e17c
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-01 15:37:40 -0700

    Majority Element

---
+++ Majority Element/Method.cpp
@@ -0,0 +1,32 @@
+//Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+//
+//You may assume that the array is non-empty and the majority element always exist in the array.
+
+
+//先假设第一个数是众数，记录count = 1, 以后每一次遇到一个数，如果不是当前众数，就--count, 如果count小于0了就要交换众数，设为当前数，再把count设为1.最后返回当前众数。
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int maj = nums[0], count = 1;
+        
+        for(int i = 1; i < nums.size(); ++i) {
+            if(nums[i] == maj) {
+                if(++count>0) {
+                    maj = nums[i];
+                }
+            }
+            else {
+                if(--count<0) {
+                    maj = nums[i];
+                    count = 1;
+                }
+            }
+            
+        }
+        
+        return maj;
+        
+    }
+};
