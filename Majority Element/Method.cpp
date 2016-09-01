d884e100b7a6069ef78da3d688b02200ecd140b7
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-01 15:40:50 -0700

    Majority Element

--- Majority Element/Method.cpp
+++ Majority Element/Method.cpp
@@ -3,7 +3,7 @@
 //You may assume that the array is non-empty and the majority element always exist in the array.
 
 
-//先假设第一个数是众数，记录count = 1, 以后每一次遇到一个数，如果不是当前众数，就--count, 如果count小于0了就要交换众数，设为当前数，再把count设为1.最后返回当前众数。
+//先假设第一个数是众数，记录count = 1, 以后每一次遇到一个数，如果不是当前众数，就--count, 如果count小于0了就要交换众数，设为当前数，再把count设为1.最后返回当前众数。该方法仅对唯一存在众数有效。
 
 class Solution {
 public:
