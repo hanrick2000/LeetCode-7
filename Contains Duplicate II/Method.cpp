52030e793a8592a05f2d58375c9af5c53d7eb514
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-04 16:06:56 -0700

    Contains Duplicate II

---
+++ Contains Duplicate II/Method.cpp
@@ -0,0 +1,23 @@
+//Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+
+//很简单，不要想复杂了。只要每一次记录该数出现的位置，然后下次遇到这个数，看看两个索引是否差值小于k。
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mymap;
+        
+        for(int i = 0; i < nums.size(); ++i) {
+            if(mymap.find(nums[i]) == mymap.end()) {
+                mymap[nums[i]] = i;
+            }
+            
+            else {
+               if(i - mymap[nums[i]] <= k) return true;
+               mymap[nums[i]] = i;
+            }
+        }
+        
+        return false;
+    }
+};
