7962fcd2fecc43e58799e5d381ea9b5130c22042
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-06 00:25:07 -0700

    Power of Two

--- Power of Two/Method.cpp
+++ Power of Two/Method.cpp
@@ -1,19 +1,19 @@
 //Given an integer, write a function to determine if it is a power of two.
 //
 //傻逼题
-//class Solution {
-//public:
-//    bool isPowerOfTwo(int n) {
-//        
-//        int mask = 1;
-//        
-//        for(int i = 0; i < 31; ++i) {
-//            if(n == mask) return true;
-//            mask<<=1;
-//            
-//        }
-//        
-//        return false;
-//        
-//    }
-//};
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        int mask = 1;
+        
+        for(int i = 0; i < 31; ++i) {
+            if(n == mask) return true;
+            mask<<=1;
+            
+        }
+        
+        return false;
+        
+    }
+};
