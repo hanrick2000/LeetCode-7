9e069dc27751ebee8405063e89f698f823c939db
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-12 15:57:17 -0700

    Range Sum Query 2D - Immutable

---
+++ Range Sum Query 2D - Immutable/Method.cpp
@@ -0,0 +1,56 @@
+//Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+//
+//Range Sum Query 2D
+//The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+//
+//Example:
+//Given matrix = [
+//  [3, 0, 1, 4, 2],
+//  [5, 6, 3, 2, 1],
+//  [1, 2, 0, 1, 5],
+//  [4, 1, 0, 1, 7],
+//  [1, 0, 3, 0, 5]
+//]
+//
+//sumRegion(2, 1, 4, 3) -> 8
+//sumRegion(1, 1, 2, 2) -> 11
+//sumRegion(1, 2, 2, 4) -> 12
+//Note:
+//You may assume that the matrix does not change.
+//There are many calls to sumRegion function.
+//You may assume that row1 ≤ row2 and col1 ≤ col2.
+
+
+与I一样还是一个二微矩阵[i][j]记录nums[0][0]到[i][j]之和，为了不用做过多判断，该二维矩阵可以比原来的多一行一列。这样返回可以用 mymatrix[row2+1][col2+1] + mymatrix[row1][col1] - mymatrix[row2+1][col1] - mymatrix[row1][col2+1];得到结果。
+
+class NumMatrix {
+    
+    vector<vector<int>> mymatrix;
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        if(!matrix.size() || !matrix[0].size()) return;
+        mymatrix.assign(matrix.size()+1, vector<int>(matrix[0].size()+1));
+        
+        for(int i = 0; i < matrix.size(); ++i) {
+            int rowSum = 0;
+            
+            for(int j = 0; j < matrix[0].size(); ++j) {
+                
+                rowSum += matrix[i][j];
+                mymatrix[i+1][j+1] = rowSum + mymatrix[i][j+1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        
+        
+        return mymatrix[row2+1][col2+1] + mymatrix[row1][col1] - mymatrix[row2+1][col1] - mymatrix[row1][col2+1];
+    }
+};
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
