220c0b86f9967112eda8e928ced89e027db0a557
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-09-13 22:32:01 -0700

    Trapping Rain Water

---
+++ Trapping Rain Water/Method.cpp
@@ -0,0 +1,51 @@
+//Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+//
+//For example, 
+//Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+//
+//
+//The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+//这题仔细观察，发现水位从0到塔尖位置是递增的，从尾部到塔尖也是递增的，所以从两边开始往中间走到塔顶水位是当前max, 面积要减掉塔高。
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int maxindex = -1, max = 0,res = 0;
+        
+        for(int i = 0; i < height.size(); ++i) {
+            if(height[i] > max) {
+                maxindex = i;
+                max = height[i];
+            }
+        }
+        
+        max = 0;
+        
+        for(int i = 0; i <maxindex; ++i) {
+            
+            if(height[i] > max) {
+                max = height[i];
+            }
+            else {
+                res += max - height[i];
+            }
+        }
+        
+        max = 0;
+        
+        for(int i = height.size()-1; i > maxindex; --i) {
+            
+            if(height[i] > max) {
+                max = height[i];
+            }
+            else {
+                res += max - height[i];
+            }
+        }
+        
+        return res;
+        
+    }
+};
