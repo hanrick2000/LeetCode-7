c40af28b39cfd8bdde172a75f96c8020d568cbab
qiuyangy@usc.edu <qiuyangy@usc.edu>
2016-10-03 23:32:05 -0700

    Number of Islands

--- Number of Islands/UnionFindMethod.cpp
+++ Number of Islands/UnionFindMethod.cpp
@@ -19,20 +19,13 @@
 //union-find 做法通常有以下三步，makeset, 把parent数组和size数组初始化。
 //2，union ， 3.计数。
 //这道题也一样，先把二维平面上的点对到一维数组上去，然后对于所有为1的点对右边和下面的1进行并集union操作。最后统计1时我们就看那些grid==1并且parent == 自己的点的个数，因为合并到最后总有一个根节点满足parent==自己。
+//统计也可以直接通过union find里面的更新count函数来，只要每次找到一个1，之前就++count，然后每次union成功都--count.
     
     
 class Solution {
 
 public:
 
-    //  //初始化  
-    //  void makeSet(int n)  {  
-    //      for (int i=0;i<n;i++)  {  
-    //          father[i]=i;  
-    //          num[i]=1;  
-    //      }  
-    //  }  
-       
      //查找，并伴随有路径压缩  
      int findSet(int x, vector<int> &father)  {  
          if (father[x]!=x)  {  
@@ -42,7 +35,8 @@ public:
      }  
        
      //合并，修改num的值  
-     void Union(vector<int> &father, vector<int> &size ,int a,int b)  {   
+     void Union(vector<int> &father, vector<int> &size ,int a,int b, int &total)  {  
+         
          int x=findSet(a,father);  
          int y=findSet(b,father);  
          
@@ -58,7 +52,8 @@ public:
          else  {  
              father[y]=x;  
              size[x]+=size[y];  
-         }   
+         }  
+         --total;
      }  
 
     int numIslands(vector<vector<char>>& grid) {
@@ -68,36 +63,31 @@ public:
         vector<int> father(grid.size()*grid[0].size(),-1);
         
         vector<int> size(grid.size()*grid[0].size(),1);
-
+        
+        int total = 0;
+        
         for(int i = 0; i < grid.size(); ++i) {    //初始化
             for(int j = 0; j < grid[0].size(); ++j) {
                 int index = i*grid[0].size() + j;
                 father[index] = index;
             }
         }
-
+    
         for(int i = 0; i < grid.size(); ++i) {   //union find 
             for(int j = 0; j < grid[0].size(); ++j) {
                 if(grid[i][j]=='1') {
+                    ++total;
                     int index = i*grid[0].size() + j;
                     if(i + 1 < grid.size() && grid[i+1][j] == '1')  {
-                        Union(father, size, index, index+grid[0].size());
+                        Union(father, size, index, index+grid[0].size(),total);
                     }
                     if(j + 1 < grid[0].size() && grid[i][j+1] == '1') {
-                        Union(father, size, index, index+1);
+                        Union(father, size, index, index+1, total);
                     }
                 }
             }
         }
-
-        int count = 0;    //统计
-        for(int i = 0; i < grid.size(); ++i) {
-            for(int j = 0; j < grid[0].size(); ++j) {
-                int index = i*grid[0].size() + j;
-                if(grid[i][j] == '1' && findSet(index, father) == index) ++count;
-            }
-        }
         
-        return count;
+        return total;
     }
 };
